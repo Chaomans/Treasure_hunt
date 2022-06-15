4ddfd254f75b15121540ad6f13f6bfe5eb3ba6d6
Chaomans <34480159+Chaomans@users.noreply.github.com>
2022-06-15 18:30:36 +0200

    refactor: Start functional approach

---
+++ hunt/hunt.js
@@ -0,0 +1,29 @@
+const {readFileSync} = require('fs')
+
+function verifyPath(){
+
+}
+
+function getContent(path){
+    return readFileSync(path, {encoding: 'utf8', flag: 'r'});
+}
+
+function createMap(){
+
+}
+
+function getMountains(){
+
+}
+
+function createAdventurer(){
+
+}
+
+function getAdventurers(){
+
+}
+
+function parseFile(content) {
+
+}
